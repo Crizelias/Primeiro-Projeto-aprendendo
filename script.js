5b32362242d8d2d3d7625151aa6e79e5a31516b8
Cristiana <cristiana.m.elias@gmail.com>
2024-02-27 16:44:52 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+ /*função setAttribute é para modificar um atributo*/
+  if (html.classList.contains("light")) {
+    /*Se tiver light mode, adicionar a imagem */
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    /*Se tiver sem light mode, manter a imagem normal*/
+  }
+}
